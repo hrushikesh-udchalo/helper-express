413485238bd9c67b3ecbcc97fdcbb8e58d007b9a
Hrushi <hrushikesh.makode@udchalo.com>
2021-11-30 09:40:53 +0530

    soap

--- src/soap/index.ts
+++ src/soap/index.ts
@@ -5,7 +5,21 @@ const url = 'http://www.dneonline.com/calculator.asmx?WSDL';
 
 export const SOAP = async () =>{
     const client = await createClientAsync(url);
-    const describe = await client.describe();
-    return describe;
+    const Add  = client['Add'];
+    try {
+        const result = await promisify(Add,{intA:103,intB:105});
+        return result;
+    } catch (error) {
+        return error;
+    }
 }
 
+
+const promisify = async (fun: Function, args: any) => {
+    return new Promise((resolve, reject) => {
+        fun(args, (err: any, result: any) => {
+            if (err) reject(err);
+            else resolve(result);
+        });
+    });
+};
