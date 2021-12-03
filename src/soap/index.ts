639cd9052028818cd2cfd8b0b990074bbc3f4083
Hrushi <hrushikesh.makode@udchalo.com>
2021-12-03 11:55:47 +0530

    checking promise.all with soap req add

--- src/soap/index.ts
+++ src/soap/index.ts
@@ -1,25 +1,25 @@
 const { soap } = require('strong-soap');
 
-// const url = 'http://www.dneonline.com/calculator.asmx?WSDL';
+const url = 'http://www.dneonline.com/calculator.asmx?WSDL';
 
-const url = 'http://www.webservicex.net/stockquote.asmx?WSDL'
-
-
-export const describe = async ()=>{
-    const client:any = await  createClient(url);
-    const res  = await client.describe();
+export const describe = async () => {
+    const client: any = await createClient(url);
+    const res = await client.describe();
     return res;
-}
-
+};
 
 export const Cal = {
-    add:async ()=>{
-        const client:any = await createClient(url);
-        const res = await client.Add({ intA:101, intB:109});
-        return res;
-    }
-}
-
+    add: async () => {
+        const client: any = await createClient(url);
+        const requests = [
+            { intA: 101, intB: 109 },
+            { intA: 102, intB: 103 },
+            { intA: 101, intB: 119 },
+        ];
+        const res = await Promise.all(requests.map(async req => await client.Add(req)));
+        return res.map(res=>res.result);
+    },
+};
 
 const createClient = (url: string) => {
     return new Promise((resolve, reject) =>
